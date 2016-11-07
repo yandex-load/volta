01e8a7155c7004dcdc5c3f7fa7f21660edaec019
Timur Torubarov <netort@yandex-team.ru>
2016-11-07 18:21:32 +0300

    Added logcat handler

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='volta',
-    version='0.0.9',
+    version='0.0.10',
     description='yandex package for mobile energy consumption measurements',
     longer_description='''
 yandex package for mobile energy consumption measurements
