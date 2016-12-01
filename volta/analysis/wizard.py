d81c28baecc6698ef791689e802ab31ec4a47d3d
Timur Torubarov <netort@yandex-team.ru>
2016-12-01 15:37:19 +0300

    volta wizard improvements

--- volta/analysis/wizard.py
+++ volta/analysis/wizard.py
@@ -5,6 +5,7 @@ import logging
 import sqlite3
 import os
 import usb
+import subprocess
 
 
 logger = logging.getLogger(__name__)
@@ -32,13 +33,13 @@ def isPhoneConnected():
     conn = sqlite3.connect('usb_list.db')
     c = conn.cursor()
     devices = usb.core.find(find_all=1)
-
     phones = []
     for device in devices:
         known_phones = [
             u'SAMSUNG_Android',
             u'iPhone',
-            u'Android'
+            u'Android',
+            u'Nexus 5X'
         ]
         if not device.product in known_phones:
             continue
@@ -61,14 +62,66 @@ def isPhoneConnected():
         return False
     return False
 
+def isPhoneDisconnected():
+    logger.info("Отключите телефон от USB...")
+    conn = sqlite3.connect('usb_list.db')
+    c = conn.cursor()
+    devices = usb.core.find(find_all=1)
+    phones = []
+    for device in devices:
+        known_phones = [
+            u'SAMSUNG_Android',
+            u'iPhone',
+            u'Android',
+            u'Nexus 5X'
+        ]
+        if not device.product in known_phones:
+            continue
+        else:
+            phones.append(device)
+    if len(phones) >= 1 :
+        q = 'SELECT name FROM devices WHERE manufacturer_id="{man_id}" AND id="{device_id}"'.format(
+            man_id=format(phones[0].idVendor, '04x'),
+            device_id=format(phones[0].idProduct, '04x'),
+        )
+        c.execute(q)
+        try:
+            device_name = c.fetchone()[0].encode('utf-8')
+        except:
+            device_name = 'Unknown device'
+        logging.info('Найден телефон: %s. id: %s', device_name, phones[0].serial_number.encode('utf-8'))
+        return False
+    elif len(phones) == 0:
+        logging.info('Телефон отключён. Запускаем тест.')
+        return True
+    return False
+
 def main():
     logging.basicConfig(
         level="INFO",
         format='%(asctime)s [%(levelname)s] [volta wizard] %(filename)s:%(lineno)d %(message)s'
     )
     logger.info("Volta wizard started")
-    #EventPoller(isUsbConnected)
+    EventPoller(isUsbConnected)
     EventPoller(isPhoneConnected)
+    logging.info('Введите длительность теста в секундах (сколько секунд коробочка будет записывать данные тока)')
+    test_duration = raw_input()
+    logging.info('Принято. Длительность теста будет %s секунд', test_duration)
+    EventPoller(isPhoneDisconnected)
+    test = subprocess.Popen('python grab.py -s {duration}'.format(
+        duration=test_duration
+        ),
+        shell=True
+    )
+    logging.info('Тест уже начался. Не забудьте помигать на телефоне фонариком!')
+    rc = test.wait()
+    logging.info('Return code: %s', rc)
+    logging.info('TODO: Здесь будет сохранялка логов с телефона')
+    logging.info('TODO: Здесь будет заливка логов коробки и эвентов телефона в Лунапарк')
+    logging.info('Volta wizard finished')
+
+
+
 
 if __name__ == "__main__":
     main()
