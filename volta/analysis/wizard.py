116477424efa01ec3a5d5d56b8a0b6d917daf571
Timur Torubarov <netort@yandex-team.ru>
2016-12-23 16:16:11 +0300

    bugfixes for phones

--- volta/analysis/wizard.py
+++ volta/analysis/wizard.py
@@ -135,13 +135,16 @@ class PhoneWorker(object):
 
     def isPhoneDisconnected(self):
         logger.info("Отключите телефон от USB...")
-        devices = usb.core.find(find_all=1)
         phones = []
-        for device in devices:
-            if not device.product in self.known_phones:
-                continue
-            else:
-                phones.append(device)
+        for device in usb.core.find(find_all=1):
+            try:
+                if device.product in self.known_phones:
+                    logger.info('Found phone: %s', device.product)
+                    phones.append(device)
+                else:
+                    continue
+            except:
+                logger.warning('Unable to detect device product', exc_info=True)
         if len(phones) >= 1 :
             q = 'SELECT name FROM devices WHERE manufacturer_id="{man_id}" AND id="{device_id}"'.format(
                 man_id=format(phones[0].idVendor, '04x'),
