49006f9eb932f946031bada54bde61d4b7edbbb6
Timur Torubarov <netort@yandex-team.ru>
2017-03-28 18:18:49 +0300

    VoltaBox binary refactoring

--- volta/Boxes/box_binary.py
+++ volta/Boxes/box_binary.py
@@ -1,4 +1,4 @@
-""" 500Hz Volta Box
+""" Binary Volta box
 """
 import serial
 import logging
@@ -6,6 +6,7 @@ import queue
 import pandas as pd
 import time
 import numpy as np
+import json
 from volta.common.interfaces import VoltaBox
 from volta.common.util import Drain
 
@@ -13,25 +14,26 @@ logger = logging.getLogger(__name__)
 
 
 dtypes = {
-    'current': np.float64
+    'current': np.uint16
 }
 
 box_columns = ['current']
 
 
-class VoltaBox500Hz(VoltaBox):
+class VoltaBoxBinary(VoltaBox):
     def __init__(self, config):
         VoltaBox.__init__(self, config)
         self.grab_timeout = config.get('grab_timeout', 1)
         self.device = config.get('device', '/dev/cu.wchusbserial1420')
-        self.sample_rate = 500
-        self.baud_rate = 115200
+        self.sample_rate = None
+        self.baud_rate = 230400
         self.chop_ratio = 0.1
         self.volta_serial = serial.Serial(self.device, self.baud_rate, timeout=self.grab_timeout)
         logger.debug('serial initialized: %s', self.volta_serial)
 
     def start_test(self, results):
-        """ pipeline
+        """ handshake w/ device, get samplerate
+            pipeline
                 read serial data ->
                 chop by samplerate w/ ratio ->
                 make pandas DataFrame ->
@@ -43,17 +45,22 @@ class VoltaBox500Hz(VoltaBox):
             puts pandas DataFrame to specified queue
         """
 
+        # handshake
+        while self.volta_serial.readline() != "VOLTAHELLO\n":
+            pass
+
+        volta_spec = json.loads(self.volta_serial.readline())
+        self.sample_rate = volta_spec["sps"]
+
         # clean up dirty buffer
-        for _ in range(500):
-            self.volta_serial.readline()
+        self.volta_serial.read(self.sample_rate*2)
 
         self.pipeline = Drain(
             TimeChopper(
-                BoxPlainTextReader(
-                    self.volta_serial
+                BoxBinaryReader(
+                    self.volta_serial, self.sample_rate
                 ),
-                self.sample_rate,
-                self.chop_ratio
+                self.sample_rate, self.chop_ratio
             ),
             results
         )
@@ -68,35 +75,26 @@ class VoltaBox500Hz(VoltaBox):
 
 def string_to_np(data):
     start_time = time.time()
-    chunk = np.fromstring(data, dtype=float, sep='\n')
+    chunk = np.fromstring(data, dtype=np.uint16).astype(np.float32)
     logger.debug("Chunk decode time: %.2fms", (time.time() - start_time) * 1000)
     return chunk
 
 
-class BoxPlainTextReader(object):
+class BoxBinaryReader(object):
     """
     Read chunks from source, convert and return numpy.array
     """
 
-    def __init__(self, source, cache_size=1024 * 1024 * 10):
+    def __init__(self, source, sample_rate):
         self.closed = False
-        self.cache_size = cache_size
         self.source = source
+        self.sample_rate = sample_rate
         self.buffer = ""
 
     def _read_chunk(self):
-        data = self.source.read(self.cache_size)
+        data = self.source.read(self.sample_rate * 2 * 10)
         if data:
-            parts = data.rsplit('\n', 1)
-            if len(parts) > 1:
-                ready_chunk = self.buffer + parts[0] + '\n'
-                self.buffer = parts[1]
-                return string_to_np(ready_chunk)
-            else:
-                self.buffer += parts[0]
-        else:
-            self.buffer += self.source.readline()
-        return None
+            return string_to_np(data)
 
     def __iter__(self):
         while not self.closed:
@@ -137,19 +135,19 @@ def main():
     logging.basicConfig(
         level="DEBUG",
         format='%(asctime)s [%(levelname)s] [Volta 500hz] %(filename)s:%(lineno)d %(message)s')
-    logger.info("Volta 500 hz box ")
+    logger.info("Volta Binary Box")
     cfg = {
         'device': '/dev/cu.wchusbserial1420'
     }
-    worker = VoltaBox500Hz(cfg)
+    worker = VoltaBoxBinary(cfg)
     logger.info('worker args: %s', worker.__dict__)
     q = queue.Queue()
     worker.start_test(q)
-    time.sleep(10)
+    time.sleep(15)
     logger.info('test finishing...')
     worker.end_test()
     logger.info('Queue size after test: %s', q.qsize())
-    logger.info('Sample size: %s', q.get())
+    logger.info('Sample: %s', q.get())
     logger.info('test finished')
 
 if __name__ == "__main__":
