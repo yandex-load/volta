0bdf5ead4f4038942bd394af5e4994a731fa0583
Timur Torubarov <netort@yandex-team.ru>
2017-04-05 20:51:40 +0300

    install lightning apk

--- volta/Phones/android.py
+++ volta/Phones/android.py
@@ -6,7 +6,7 @@ import os
 import time
 import queue
 from volta.common.interfaces import Phone
-from volta.common.util import popen, Drain
+from volta.common.util import popen, Drain, execute
 from volta.common.resource import manager as resource
 from volta.Boxes.box_binary import VoltaBoxBinary
 
@@ -14,24 +14,32 @@ from volta.Boxes.box_binary import VoltaBoxBinary
 logger = logging.getLogger(__name__)
 
 
+lightning_apk_fullname = "net.yandex.overload.lightning"
+
+
 class AndroidPhone(Phone):
     def __init__(self, config, volta):
-        Phone.__init__(self, config)
+        Phone.__init__(self, config, volta)
         self.volta = volta
         self.source = config.get('source', '00dc3419957ba583')
         self.lightning_apk_path = config.get('lightning', 'binary/lightning.apk')
-        self.adb_cmd = "adb -s {dev} logcat".format(dev=self.source)
+        self.lightning_apk_fname = None
 
     def prepare(self):
         """
         pipeline:
-            install ligtning
+            install lightning
             install apks
             clean log
         """
         # test
         self.lightning_apk_fname = resource.get_opener(self.lightning_apk_path).get_filename
-        logger.info('Lightning: %s', self.lightning_apk_fname)
+
+        logger.info('Uninstalling lightning...')
+        execute("adb -s {dev} uninstall {apk}".format(dev=self.source, apk=lightning_apk_fullname))
+        logger.info('Installing lightning apk...')
+        execute("adb -s {dev} install {apk}".format(dev=self.source, apk=self.lightning_apk_fname))
+
 
     def start(self):
         """
@@ -65,7 +73,7 @@ class AndroidPhone(Phone):
 def main():
     logging.basicConfig(
         level="DEBUG",
-        format='%(asctime)s [%(levelname)s] [Volta 500hz] %(filename)s:%(lineno)d %(message)s')
+        format='%(asctime)s [%(levelname)s] [Volta Phone Android] %(filename)s:%(lineno)d %(message)s')
     logger.info("Volta Phone Anroid ")
     cfg_volta = {
     }
