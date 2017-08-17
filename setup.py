714e6cfe8869485defdc9a13a44438f4035940ff
Timur Torubarov <netort@yandex-team.ru>
2017-08-17 18:52:25 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='volta',
-    version='0.1.7',
+    version='0.1.8',
     description='yandex package for mobile energy consumption measurements',
     longer_description='''
 yandex package for mobile energy consumption measurements
