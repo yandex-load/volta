198fdc34d986e82dd963b0b1f5708d5afd3cf65e
Alexey Lavrenuke <direvius@gmail.com>
2017-08-18 15:53:09 +0300

    fix router if dataframe is None

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='volta',
-    version='0.1.9',
+    version='0.1.10',
     description='yandex package for mobile energy consumption measurements',
     longer_description='''
 yandex package for mobile energy consumption measurements
