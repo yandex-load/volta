60eef9683421e09cfc74579f5e496a2f0e58c1a5
netort <netort@yandex-team.ru>
2018-03-13 17:09:19 +0300

    bugfix logger

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='volta',
-    version='0.5.9',
+    version='0.5.9-1',
     description='yandex package for mobile energy consumption measurements',
     longer_description='''
 yandex package for mobile energy consumption measurements
